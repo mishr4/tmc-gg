49522d39b9e2e463b336f16a7e423a6f1ba76ac1
Alexander Mishra <alexnmishra@gmail.com>
2026-07-21 17:58:24 -0700

    Add Mavion Go system tray controls

--- mavion-student-desktop/main.cjs
+++ mavion-student-desktop/main.cjs
@@ -1,8 +1,24 @@
-const { app, BrowserWindow, ipcMain, Notification } = require('electron');
+const { app, BrowserWindow, ipcMain, Notification, Tray, Menu } = require('electron');
 const path = require('path');
 
 let controlWindow;
 let lockWindow;
+let tray;
+let quitting = false;
+const iconPath = path.join(__dirname, 'assets', 'mavion-lock.ico');
+function showControl() { controlWindow.show(); controlWindow.focus(); }
+function showLock() { lockWindow.show(); lockWindow.focus(); }
+function createTray() {
+  tray = new Tray(iconPath);
+  tray.setToolTip('Mavion Go Lock');
+  tray.setContextMenu(Menu.buildFromTemplate([
+    { label: 'Lock desktop', click: showLock },
+    { label: 'Open Mavion Go', click: showControl },
+    { type: 'separator' },
+    { label: 'Quit', click: () => { quitting = true; app.quit(); } }
+  ]));
+  tray.on('click', showControl);
+}
 function createLockWindow() {
   lockWindow = new BrowserWindow({
     fullscreen: true,
@@ -25,13 +41,17 @@ function createControlWindow() {
     backgroundColor: '#111318',
     autoHideMenuBar: true,
     title: 'Mavion Go',
-    icon: path.join(__dirname, 'assets', 'mavion-lock.ico'),
+    icon: iconPath,
     webPreferences: { contextIsolation: true, nodeIntegration: false, sandbox: true, preload: path.join(__dirname, 'preload.cjs') }
   });
   controlWindow.loadFile(path.join(__dirname, 'renderer', 'control.html'));
+  controlWindow.on('close', event => { if (!quitting) { event.preventDefault(); controlWindow.hide(); } });
+  if (app.getLoginItemSettings().wasOpenedAtLogin) controlWindow.once('ready-to-show', () => controlWindow.hide());
 }
-ipcMain.handle('activate-lock', () => { lockWindow.show(); lockWindow.focus(); });
-ipcMain.handle('release-lock', () => { lockWindow.hide(); controlWindow.show(); controlWindow.focus(); });
+ipcMain.handle('activate-lock', showLock);
+ipcMain.handle('release-lock', () => { lockWindow.hide(); showControl(); });
 ipcMain.handle('test-notification', () => new Notification({ title: 'Mavion Go', body: 'Test notification from Mavion Go.' }).show());
-app.whenReady().then(() => { createControlWindow(); createLockWindow(); app.on('activate', () => { controlWindow.show(); controlWindow.focus(); }); });
-app.on('window-all-closed', event => event.preventDefault());
+ipcMain.handle('get-startup', () => app.getLoginItemSettings().openAtLogin);
+ipcMain.handle('set-startup', (_, enabled) => app.setLoginItemSettings({ openAtLogin: Boolean(enabled) }));
+app.whenReady().then(() => { createControlWindow(); createLockWindow(); createTray(); app.on('activate', showControl); });
+app.on('window-all-closed', event => { if (!quitting) event.preventDefault(); });
--- mavion-student-desktop/preload.cjs
+++ mavion-student-desktop/preload.cjs
@@ -2,5 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('mavion', {
   activateLock: () => ipcRenderer.invoke('activate-lock'),
   releaseLock: () => ipcRenderer.invoke('release-lock'),
-  testNotification: () => ipcRenderer.invoke('test-notification')
+  testNotification: () => ipcRenderer.invoke('test-notification'),
+  getStartup: () => ipcRenderer.invoke('get-startup'),
+  setStartup: enabled => ipcRenderer.invoke('set-startup', enabled)
 });
