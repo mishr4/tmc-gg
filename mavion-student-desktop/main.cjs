731fc38f94c9c3b92dbe264fee694269eae2909f
Alexander Mishra <alexnmishra@gmail.com>
2026-07-21 18:27:58 -0700

    Use system idle time for Mavion Go lock

--- mavion-student-desktop/main.cjs
+++ mavion-student-desktop/main.cjs
@@ -1,13 +1,17 @@
-const { app, BrowserWindow, ipcMain, Notification, Tray, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain, Notification, Tray, Menu, powerMonitor } = require('electron');
 const path = require('path');
 
 let controlWindow;
 let lockWindow;
 let tray;
 let quitting = false;
+let idleLockEnabled = true;
 const iconPath = path.join(__dirname, 'assets', 'mavion-lock.ico');
 function showControl() { controlWindow.show(); controlWindow.focus(); }
 function showLock() { lockWindow.show(); lockWindow.focus(); }
+function checkSystemIdle() {
+  if (idleLockEnabled && !lockWindow.isVisible() && powerMonitor.getSystemIdleTime() >= 300) showLock();
+}
 function createTray() {
   tray = new Tray(iconPath);
   tray.setToolTip('Mavion Go Lock');
@@ -49,9 +53,11 @@ function createControlWindow() {
   if (app.getLoginItemSettings().wasOpenedAtLogin) controlWindow.once('ready-to-show', () => controlWindow.hide());
 }
 ipcMain.handle('activate-lock', showLock);
-ipcMain.handle('release-lock', () => { lockWindow.hide(); showControl(); });
+ipcMain.handle('release-lock', () => { lockWindow.hide(); });
 ipcMain.handle('test-notification', () => new Notification({ title: 'Mavion Go', body: 'Test notification from Mavion Go.' }).show());
 ipcMain.handle('get-startup', () => app.getLoginItemSettings().openAtLogin);
 ipcMain.handle('set-startup', (_, enabled) => app.setLoginItemSettings({ openAtLogin: Boolean(enabled) }));
-app.whenReady().then(() => { createControlWindow(); createLockWindow(); createTray(); app.on('activate', showControl); });
+ipcMain.handle('get-idle-lock', () => idleLockEnabled);
+ipcMain.handle('set-idle-lock', (_, enabled) => { idleLockEnabled = Boolean(enabled); });
+app.whenReady().then(() => { createControlWindow(); createLockWindow(); createTray(); setInterval(checkSystemIdle, 10000); app.on('activate', showControl); });
 app.on('window-all-closed', event => { if (!quitting) event.preventDefault(); });
--- mavion-student-desktop/preload.cjs
+++ mavion-student-desktop/preload.cjs
@@ -4,5 +4,7 @@ contextBridge.exposeInMainWorld('mavion', {
   releaseLock: () => ipcRenderer.invoke('release-lock'),
   testNotification: () => ipcRenderer.invoke('test-notification'),
   getStartup: () => ipcRenderer.invoke('get-startup'),
-  setStartup: enabled => ipcRenderer.invoke('set-startup', enabled)
+  setStartup: enabled => ipcRenderer.invoke('set-startup', enabled),
+  getIdleLock: () => ipcRenderer.invoke('get-idle-lock'),
+  setIdleLock: enabled => ipcRenderer.invoke('set-idle-lock', enabled)
 });
